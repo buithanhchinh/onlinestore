dde5fcf99bcb149bcd8df8f11e542335823cf990
Chinh Bui <chinh.bui@orientsoftware.net>
2017-07-05 07:30:34 +0700

    buidling authentication service part 1

---
+++ onlinestorevn/src/app/common/message.constants.ts
@@ -0,0 +1,10 @@
+
+export class MessageConstants {
+
+    public static SYSTEM_ERROR_MSG = "Failed to connect to server";
+    public static CONFIRM_DELETE_MSG = "Are you sure you want to delete the selected item?";
+    public static LOGIN_AGAIN_MSG = "Please login again!";
+    public static CREATE_OK_MSG = "Successfully created";
+    public static UPDATE_OK_MSG = "Successfully updated";
+    public static DELETE_OK_MSG = "Successfully deleted";
+}
---
+++ onlinestorevn/src/app/common/uri.constants.ts
@@ -0,0 +1,5 @@
+export class UriConstants{
+    
+    public static LOGIN = "/login";
+	public static HOME = "/main/home/index"
+}
